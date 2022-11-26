44c3fb3d28b82ed9d1964cfb573f3c1ced0f4987
George Tsotsos <gtsotsos@icloud.com>
2022-11-26 00:27:28 -0500

    clean up css classes and simplify layout with tailwind, customise mobile layout

--- src/components/Education/EducationItem.tsx
+++ src/components/Education/EducationItem.tsx
@@ -4,8 +4,8 @@ import {SchoolTypes} from "../../data/education";
 export const EducationItem = ({ graduation_year, school, location, description }: SchoolTypes) => {
     return (
         <div className="grid subgrid">
-            <div className="subleft">{graduation_year}</div>
-            <div className="subright">
+            <div>{graduation_year}</div>
+            <div>
                 <h4 className="font-bold text-md">{school}</h4>
                 <h5 className="italic text-sm mb-5">{location}</h5>
                 <p>{description}</p>
